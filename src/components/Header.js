465702755f958e37a31407ef08320cc45f20b552
valeriavalles <valeria.sv@outlook.es>
2018-03-14 09:18:29 -0500

    componente

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Header = () => (
+    <div>
+       <h1>Mis hobies</h1> 
+       <hr/> 
+       <ul>
+           <li>escuchar musica</li>
+           <li>pinta</li>
+           <li>cantar</li>
+        </ul>     
+    </div>
+  )
+  
+  export default Header
